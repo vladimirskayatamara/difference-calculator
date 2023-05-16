50069580151533ef5e3bce687abbdbbd3706c3ce
vladimirskayatamara <vladimirskayatamara@gmail.com>
2023-05-16 17:33:41 +0700

    Added tool to compare 2 files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,8 +10,8 @@ program
     .description('Compares two configuration files and shows a difference.')
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2) => {
-      console.log(gendiff(filepath1, filepath2));
+    .action((filepath1, filepath2, options) => {
+      console.log(genDiff(filepath1, filepath2, options.format));
     });
     
 program.parse(process.argv);
